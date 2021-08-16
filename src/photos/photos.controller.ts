c97a745790e8e9529acd0508ecbdfedf16599566
rugaba <rugaba@gmail.com>
2021-08-16 16:26:08 +0200

    final

--- src/photos/photos.controller.ts
+++ src/photos/photos.controller.ts
@@ -2,7 +2,6 @@ import {
   Controller,
   Get,
   Post,
-  Body,
   Patch,
   Param,
   Delete,
@@ -12,25 +11,39 @@ import {
 } from '@nestjs/common';
 import { PhotosService } from './photos.service';
 import { CreatePhotoDto } from './dto/create-photo.dto';
-import { UpdatePhotoDto } from './dto/update-photo.dto';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiConsumes,
+  ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { Response } from 'express';
+import { genericResponse } from './../config/genericResponse';
+import { ApiFile } from './ApiFile';
+import { PhotosEntity } from './entities/photo.entity';
 
 @ApiTags('PHOTOS')
 @Controller('photos')
 export class PhotosController {
   constructor(private readonly photosService: PhotosService) {}
 
-  @Get(':fileid')
+  @Get('upload/:filePath')
   async sendFile(
-    @Param('fileid') fileId: string,
+    @Param('filePath') fileId: string,
     @Res() res: Response,
   ): Promise<any> {
     res.sendFile(fileId, { root: 'photo' });
   }
 
   @Post()
+  @ApiOkResponse({ description: genericResponse.ok.response })
+  @ApiInternalServerErrorResponse({
+    description: genericResponse.serverKo.response,
+  })
+  @ApiConsumes('multipart/form-data')
+  @ApiFile('photo')
   @UseInterceptors(FilesInterceptor('photo'))
   create(@UploadedFiles() file: Express.Multer.File) {
     const photoDto = new CreatePhotoDto();
@@ -41,21 +54,53 @@ export class PhotosController {
   }
 
   @Get()
+  @ApiOkResponse({
+    description: genericResponse.ok.response,
+    type: [PhotosEntity],
+  })
+  @ApiNotFoundResponse({ description: genericResponse.notFound.response })
+  @ApiInternalServerErrorResponse({
+    description: genericResponse.serverKo.response,
+  })
   findAll() {
     return this.photosService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({
+    description: genericResponse.ok.response,
+    type: PhotosEntity,
+  })
+  @ApiNotFoundResponse({ description: genericResponse.notFound.response })
+  @ApiInternalServerErrorResponse({
+    description: genericResponse.serverKo.response,
+  })
   findOne(@Param('id') id: string) {
     return this.photosService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePhotoDto: UpdatePhotoDto) {
-    return this.photosService.update(+id, updatePhotoDto);
+  @ApiOkResponse({ description: genericResponse.ok.response })
+  @ApiInternalServerErrorResponse({
+    description: genericResponse.serverKo.response,
+  })
+  @ApiConsumes('multipart/form-data')
+  @ApiFile('photo')
+  @UseInterceptors(FilesInterceptor('photo'))
+  update(@Param('id') id: string, @UploadedFiles() file: Express.Multer.File) {
+    const photoUdateDto = new CreatePhotoDto();
+    photoUdateDto.createAt = new Date();
+    photoUdateDto.fileName = file[0].filename;
+    photoUdateDto.pathFile = 'http://localhost:3000/photos/' + file[0].filename;
+    return this.photosService.update(+id, photoUdateDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ description: genericResponse.ok.response })
+  @ApiNotFoundResponse({ description: genericResponse.notFound.response })
+  @ApiInternalServerErrorResponse({
+    description: genericResponse.serverKo.response,
+  })
   remove(@Param('id') id: string) {
     return this.photosService.remove(+id);
   }
--- src/photos/photos.service.ts
+++ src/photos/photos.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreatePhotoDto } from './dto/create-photo.dto';
@@ -17,19 +21,35 @@ export class PhotosService {
     return photo;
   }
 
-  findAll() {
-    return `This action returns all photos`;
+  async findAll() {
+    const photos = await this.phoptosRepository.find();
+    if (photos.length <= 0) {
+      throw new NotFoundException();
+    }
+    return photos;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} photo`;
+  async findOne(id: number) {
+    const photo = await this.phoptosRepository.findOne(id);
+    if (!photo) {
+      throw new NotFoundException();
+    }
+    return photo;
   }
 
-  update(id: number, updatePhotoDto: UpdatePhotoDto) {
-    return `This action updates a #${id} photo`;
+  async update(id: number, updatePhotoDto: UpdatePhotoDto) {
+    const affected = await this.phoptosRepository.update(id, updatePhotoDto);
+    if (affected.affected !== 1) {
+      throw new InternalServerErrorException();
+    }
+    return affected;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} photo`;
+  async remove(id: number) {
+    const affected = await this.phoptosRepository.delete(id);
+    if (affected.affected !== 1) {
+      throw new InternalServerErrorException();
+    }
+    return affected;
   }
 }
